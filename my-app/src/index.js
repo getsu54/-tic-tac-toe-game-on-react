6e652214ded651d47bfe8adbd2b5d1343634fd0b
getsu54 <udovichenkodaniil@gmail.com>
2022-07-21 16:42:53 +0300

    tic-tac-toe
    tic-tac-toe game , based on React

---
+++ my-app/src/index.js
@@ -0,0 +1,182 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+//SQUARE
+ function Square (props){
+      return (
+        <button className="square" onClick= {props.onClick}>
+          {props.value}
+        </button>
+      );
+    }
+  
+
+  //BOARD
+  class Board extends React.Component {
+
+    constructor (props){ //конструктор нужен для инициализации состояния
+      super(props);
+        this.state={
+          squares : Array(9).fill(null),
+          xIsNext:true
+        };
+      }
+
+      handleClick(i){
+        const squares = this.state.squares.slice();
+        if (calctulateWinner (squares)|| square [i]){
+          return;
+        }
+        squares[i] = this.state.xIsNext? 'X' : 'O';
+        this.setState({
+          squares: squares,
+          xIsNext: !this.state.xIsNext
+        });
+      }
+    
+    renderSquare(i) {
+      return (
+      <Square 
+      value={this.state.squares[i]}
+      onClick={()=> this.handleClick(i)}//обрабатывает событие
+      />
+      );
+    }
+
+  // board-rows
+    render() {
+      const winner = calculateWinner (this.state.squares);
+      let status;
+      if (winner ){
+        status = 'Победитель' + winner ;
+  
+        
+      }else{
+        status = 'Следующий игрок' + (this.state.xIsNext? 'X' : 'O');
+      }
+  
+      return (
+        <div>
+
+          <div className="status">{status}</div>
+          
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+
+
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+
+        </div>
+      );
+    }
+  }
+  
+//GAME
+
+  class Game extends React.Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        history: [{
+          squares: Array(9).fill(null),
+        }],
+        xIsNext: true
+      };
+    }
+
+
+
+    render() {
+      return (
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
+      );
+    }
+  }
+  
+
+  function calculateWinner (squares ){
+
+    const lines = [
+
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+  
+  
+
+  history = [
+// Перед первым ходом
+{
+  squares : [
+
+    null,null,null,
+    null,null,null,
+    null,null,null,
+  ]
+},
+// После первого хода
+
+{
+squares : [
+  null,null,null,
+  null,'X', null,
+  null,null,null,
+  ]
+},
+
+//После второго хода
+
+{
+
+squares: [
+  null, null,null,
+  null, 'X' ,null,
+  null, null, 'O',
+    ]
+  },
+//...
+]
+
+
+
+  // ========================================
+  
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(<Game />);
